e1dc7874d8c936405dc78255bfb4ed658e58a310
jaewook <tjqk77@gmail.com>
2023-04-20 18:08:34 +0900

    anot

--- iiii/src/main/java/com/multicampus/hhh/testadd/testKeeping.java
+++ iiii/src/main/java/com/multicampus/hhh/testadd/testKeeping.java
@@ -1,8 +1,14 @@
 package com.multicampus.hhh.testadd;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class testKeeping {
     private Long bno;
     private String test;
