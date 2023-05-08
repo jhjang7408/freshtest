33b2709169773a4ce3be237e69579dd4049c4145
JAEWOO <khwytv39@gmail.com>
2023-05-09 00:01:01 +0900

    0508 수정,댓글 등록

--- iiii/src/main/java/com/multicampus/hhh/controller/BikeBoardReplyController.java
+++ iiii/src/main/java/com/multicampus/hhh/controller/BikeBoardReplyController.java
@@ -1,6 +1,7 @@
 package com.multicampus.hhh.controller;
 
 
+import com.multicampus.hhh.domain.MemberVO;
 import com.multicampus.hhh.dto.BikeBoardDTO;
 import com.multicampus.hhh.dto.BikeBoardReplyDTO;
 import com.multicampus.hhh.service.BikeBoardReplyService;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/bike")
 @RequiredArgsConstructor
@@ -18,14 +21,23 @@ public class BikeBoardReplyController {
 
     private final BikeBoardReplyService bikeBoardReplyService;
 
-    @PostMapping("productSingle/{bikeId}/bikereply")
-    public String register(@PathVariable int bikeid, BikeBoardDTO bikeBoardDTO, BikeBoardReplyDTO bikeBoardReplyDTO, Model model){
+    @PostMapping("productSingle/{bikeid}/bikereply")
+    public String register(@PathVariable int bikeid, BikeBoardDTO bikeBoardDTO, BikeBoardReplyDTO bikeBoardReplyDTO, Model model, HttpSession session){
         int bikereplyid = bikeBoardReplyDTO.getBikereplyid();
+        MemberVO userid = (MemberVO) session.getAttribute("loginId");
+        //'user_id' cannot be null오류 발생해서
+        if (userid != null) {
+            bikeBoardReplyDTO.setUserid(userid.getUserid());
+        } else {
+            // 로그인이 되어 있지 않은 경우
+            return "redirect:/member/signin";
+        }
+        ;
         model.addAttribute("reply", bikeBoardReplyDTO);
         model.addAttribute("bike", bikeBoardDTO);
         bikeBoardReplyService.register(bikeBoardReplyDTO);
 
-        return "bike/productSingle/" +bikeid;
+        return "redirect:/bike/productSingle/" + bikeid;
     }
 
 //    @PostMapping()
