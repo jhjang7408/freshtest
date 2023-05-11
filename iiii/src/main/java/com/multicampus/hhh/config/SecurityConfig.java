4e2e837a19871a46d80fd1375f1c6ec7a683119b
dddddddd <126403497+JWK666@users.noreply.github.com>
2023-05-11 15:51:01 +0900

    -

--- iiii/src/main/java/com/multicampus/hhh/config/SecurityConfig.java
+++ iiii/src/main/java/com/multicampus/hhh/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/member/signin")    //일반 로그인
                 .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/")
-                .failureHandler(new CustomLoginFailHandler())
+                .failureHandler(new CustomLoginFailHandler())   //로그인 실패 핸들러
                 .and()
                 .oauth2Login().loginPage("/member/signin")  //소셜 로그인
                 .userInfoEndpoint()
