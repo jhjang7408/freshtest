98b81a94347cc9d81d62a62d98cbcff7c1958e1b
JAEWOO <khwytv39@gmail.com>
2023-05-15 10:10:04 +0900

    0515 상단바 연결

--- iiii/src/main/java/com/multicampus/hhh/controller/AccBoardController.java
+++ iiii/src/main/java/com/multicampus/hhh/controller/AccBoardController.java
@@ -46,11 +46,11 @@ public class AccBoardController {
         int total = pageService.countAccBoard();
         if (nowPage == null && cntPerPage == null) {
             nowPage = "1";
-            cntPerPage = "5";
+            cntPerPage = "12";
         } else if (nowPage == null) {
             nowPage = "1";
         } else if (cntPerPage == null) {
-            cntPerPage = "5";
+            cntPerPage = "12";
         }
         vo = new PagingVO(total, Integer.parseInt(nowPage), Integer.parseInt(cntPerPage));
         model.addAttribute("paging", vo);
